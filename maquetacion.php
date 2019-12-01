2b48b96743c2d595116b1d374b03d998f0f2dc00
JORGEVEGAPADRE <55414869+JORGEVEGAPADRE@users.noreply.github.com>
2019-12-01 10:12:05 -0300

    Carrito de Compras- PHP POO, HTML5, CCS3, MYSQL PATRON: MODELO-VISTA-CONTROLADOR

---
+++ maquetacion.php
@@ -0,0 +1,106 @@
+<!DOCTYPE HTML>
+<html lang="es">
+
+<head>
+   
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Tienda de Camisetas</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+</head>
+
+<body>
+<div id="container">
+    <!--CABECERA-->
+    <header id="header">
+        <div id="logo">
+            <img src="assets/img/camiseta.png" alt="Camiseta Logo"/>
+            <a href="index.php">
+                Tienda de Camisetas
+            </a> 
+        </div>
+    </header>
+
+
+    <!--MENU-->
+    <nav id="menu">
+            <ul>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 1</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 2</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 3</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 4</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 5</a>
+                </li>                
+            </ul>
+    </nav>
+
+    <div id="content">
+
+    <!--BARRA LATERAL-->
+    <aside id="lateral">
+        <div id="login" class="block_aside">
+            <h3>Entrar a la Web</h3>
+            <form action="#" method="POST">
+                <label for="email">Email</label>
+			    <input type="email" name="email" />
+
+			    <label for="password">Contraseña</label>
+			    <input type="password" name="password" />
+
+			    <input type="submit" value="Entrar" />
+            </form>
+            <ul>
+                <li><a href="#">Mis Pedidos</a></li>
+                <li><a href="#">Gestionar Pedidos</a></li>
+                <li><a href="#">Gestionar Categorias</a></li>
+            </ul>            
+        </div>
+
+    </aside>    
+    <!--CONTENIDO CENTRAL-->
+    <div id="central">
+        <h1>Productos Destacados</h1>
+        <div class="product">
+            <img src="assets/img/camiseta.png">
+            <h2>Camiseta Azul Ancha</h2>
+            <p>30 euros</p>
+            <a href="" class="button">Comprar</a>
+        </div>
+        <div class="product">
+            <img src="assets/img/camiseta.png">
+            <h2>Camiseta Azul Ancha</h2>
+            <p>30 euros</p>
+            <a href=""class="button">Comprar</a>
+        </div>
+        <div class="product">
+            <img src="assets/img/camiseta.png">
+            <h2>Camiseta Azul Ancha</h2>
+            <p>30 euros</p>
+            <a href=""class="button">Comprar</a>
+        </div>
+    </div>
+
+    </div> 
+
+
+    <!--PIE DE PAGINA/FOOTER-->
+    <footer id="footer">
+       <p> Desarrollado Por: Ing. Jorge Vega &copy <?= date('Y');?></p> 
+    </footer>
+</div>
+</body>
+
+</html>
---
+++ views/layout/footer.php
@@ -0,0 +1,9 @@
+	</div>
+		</div>
+			<!-- PIE DE PÁGINA -->
+			<footer id="footer">
+				<p>Desarrollado por Ing. Jorge Vega &copy; <?= date('Y') ?></p>
+			</footer>
+		</div>
+	</body>
+</html>
